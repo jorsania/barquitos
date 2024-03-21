4531567f99de2d5f8164b1176b01f24b1e18cb4f
Javier Ors Anía <javier@orsania.es>
2024-03-21 23:28:31 +0100

    Cambios en Docker y configuraciones NGINX
    - Se modifican algunos archivos relacionados con la configuración Docker
      del entorno de producción para que el contenedor frontend del entorno de
      desarrollo se ejecute con el mismo usuario que invoca el script
      desarrollo.sh.
    
    - Se modifican las configuraciones del servidor NGINX de pasarela y entorno
      de producción para:
    
      - Mejorar la gestión de redirecciones buscando que funcione correctamente
        en todos los entornos y escenarios posibles, solucionando algunos
        problemas con el funcionamiento de los websockets.
    
      - Cambiar la variable DIRECCION que se estaba pasando al parámetro
        server_name de forma incorrecta por la variable DOMINIO, esta última se
        añade a los scripts desarrollo.sh y produccion.sh y se infiere a partir
        de la variable DIRECCION.

--- desarrollo.sh
+++ desarrollo.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+export USRID=$(id -u)
+export GRPID=$(id -g)
+export DOMINIO=${DIRECCION#http?(s)://}
+
 docker compose stop
 [ -x docker-compose.override.yml ] && rm docker-compose.override.yml
-ln -s docker-compose.dev.yml docker-compose.override.yml
+ln -s docker-compose.dev.yml  docker-compose.override.yml
 docker compose up -d
--- produccion.sh
+++ produccion.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+export DOMINIO=${DIRECCION#http?(s)://}
+
+
+
 docker compose stop
 [ -x docker-compose.override.yml ] && rm docker-compose.override.yml
 ln -s docker-compose.prod.yml docker-compose.override.yml
-docker compose up --build -d
+docker compose up -d --build
