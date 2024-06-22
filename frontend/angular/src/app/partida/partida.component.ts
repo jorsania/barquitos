7f36fa783e12f8652cbd8c353d2cd096861c7665
JAVIER ORS ANIA <jors@minsait.com>
2024-06-22 23:22:59 +0200

    cambios

--- frontend/angular/src/app/partida/partida.component.ts
+++ frontend/angular/src/app/partida/partida.component.ts
@@ -9,57 +9,50 @@ import { Subject, takeUntil } from 'rxjs';
   templateUrl: './partida.component.html',
   styleUrls: ['./partida.component.css'],
 })
-
-export class PartidaComponent implements OnInit {
-
+export class PartidaComponent {
   public turno?: boolean;
   public isAuthenticated = false;
   public ganador: boolean = false;
   public perdedor: boolean = false;
-  public tableroPropio: String[][];
-  public tableroOponente: String[][];
   public notificaTurnoJugador: boolean = false;
   public notificaTurnoOponente: boolean = false;
+  public tableroPropio: String[][] = Array.from({ length: 10 }, () => Array.from({ length: 10 }, () => ''));
+  public tableroOponente: String[][] = Array.from({ length: 10 }, () => Array.from({ length: 10 }, () => ''));
 
   private socket: WebSocket;
   private _destroySub$ = new Subject<void>();
 
-  constructor(
-    private autentificaService: AutentificaService
-  ) {
+  constructor(private autentificaService: AutentificaService) {
     this.socket = new WebSocket(`${environment.HOST_ADDR}partida/ws`);
-    this.tableroPropio = Array.from({ length: 10 }, () => Array.from({ length: 10 }, () => ''));
-    this.tableroOponente = Array.from({ length: 10 }, () => Array.from({ length: 10 }, () => ''));
-    this.autentificaService.isAuthenticated$.pipe(
-      takeUntil(this._destroySub$)
-    ).subscribe((isAuthenticated: boolean) => this.isAuthenticated = isAuthenticated);
-  };
+    this.socket.onmessage = this.onMessage.bind(this);
+    this.autentificaService.isAuthenticated$
+      .pipe(takeUntil(this._destroySub$))
+      .subscribe((isAuthenticated: boolean) => this.isAuthenticated = isAuthenticated);
+  }
 
   public dispara(x: number, y: number) {
     if (this.turno) this.socket.send(JSON.stringify({ "disparo": { "x": x + 1, "y": y + 1 } }))
   }
 
-  ngOnInit(): void {
-    this.socket
-      .onmessage =(event) => {
-        const msg: RespuestaJuego = JSON.parse(event.data);
-        if (msg.TableroPropio) this.tableroPropio = msg.TableroPropio;
-        if (msg.TableroOponente) this.tableroOponente = msg.TableroOponente;
-        if ((typeof msg.turno != 'undefined' && msg.turno != this.turno)) {
-          this.turno = msg.turno;
-          if (msg.turno == true) {
-            this.notificaTurnoJugador = true;
-            setTimeout(() => { this.notificaTurnoJugador = false; }, 1000);
-          } else {
-            this.notificaTurnoOponente = true;
-            setTimeout(() => { this.notificaTurnoOponente = false; }, 1000);
-          }
-        }
-        if (typeof msg.ganador != 'undefined') {
-          msg.ganador ? this.ganador = true : this.perdedor = true;
-          this.socket.close();
-        }
+  private onMessage(event: MessageEvent) {
+    const msg: RespuestaJuego = JSON.parse(event.data);
+    if (msg.TableroPropio) this.tableroPropio = msg.TableroPropio;
+    if (msg.TableroOponente) this.tableroOponente = msg.TableroOponente;
+    if (typeof msg.turno !== 'undefined' && msg.turno !== this.turno) {
+      this.turno = msg.turno;
+      if (msg.turno) {
+        this.notificaTurnoJugador = true;
+        setTimeout(() => { this.notificaTurnoJugador = false; }, 1000);
+      } else {
+        this.notificaTurnoOponente = true;
+        setTimeout(() => { this.notificaTurnoOponente = false; }, 1000);
       }
+    }
+    if (typeof msg.ganador !== 'undefined') {
+      this.ganador = msg.ganador;
+      this.perdedor = !msg.ganador;
+      this.socket.close();
+    }
   }
 
   ngOnDestroy() {
